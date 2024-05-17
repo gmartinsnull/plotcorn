69672ac97d3cc77320dbc7713b694b7a0511f0cc
gmartinsnull <gmartins.dev@gmail.com>
2024-05-16 17:49:33 -0700

    add transpile packages in next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,14 @@
 // @type {import('next').NextConfig}
 module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "covers.openlibrary.org",
-          port: "",
-          pathname: "/b/id/**",
-        },
-      ],
-    },
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "covers.openlibrary.org",
+        port: "",
+        pathname: "/b/id/**",
+      },
+    ],
+  },
+  transpilePackages: ["@packages/movies-pkg", "@packages/subjects-pkg"],
+};
